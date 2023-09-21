9585bc08a08ab0fea4511fcf384f97b796b0682e
Nahason <nahason.murithi@student.moringaschool.com>
2023-09-21 22:15:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+ customerName = `BOB`;
+ return customerName;
+};
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+const leastFavouriteCustomer ='bob';
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = 'kira'
+    return leastFavouriteCustomer;
+}
